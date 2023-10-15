771bb50838ac5e762ec862e4b15def8f6fe1702f
ruslanyar <styxrus@yandex.ru>
2023-10-15 19:33:51 +0300

    submit 1_TwoSum

---
+++ leetcode/1_TwoSum/script.ts
@@ -0,0 +1,27 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Constraints:
+
+// 2 <= nums.length <= 10^4
+// -10^9 <= nums[i] <= 10^9
+// -10^9 <= target <= 10^9
+// Only one valid answer exists.
+
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = target - num;
+    const mapVal = map.get(diff);
+    if (mapVal !== undefined) return [mapVal!, i];
+    map.set(num, i);
+  }
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // [1, 2]
