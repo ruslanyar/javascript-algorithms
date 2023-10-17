967c10160d2d4d799408ebf944abf8a629756f59
ruslanyar <styxrus@yandex.ru>
2023-10-17 11:16:29 +0300

    submit 2620

---
+++ leetcode/2620_Counter/script.ts
@@ -0,0 +1,17 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+function createCounter(n: number): () => number {
+  return () => n++;
+}
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
