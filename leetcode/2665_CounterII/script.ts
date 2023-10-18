051501e0ae83adafccf504b23fe7d3ca1b5980db
ruslanyar <styxrus@yandex.ru>
2023-10-18 20:24:00 +0300

    submit 2665

---
+++ leetcode/2665_CounterII/script.ts
@@ -0,0 +1,22 @@
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+type ReturnObj = {
+  increment: () => number;
+  decrement: () => number;
+  reset: () => number;
+};
+
+function createCounter(init: number): ReturnObj {
+  let num = init;
+  return {
+    increment: () => ++num,
+    decrement: () => --num,
+    reset: () => (num = init),
+  };
+}
