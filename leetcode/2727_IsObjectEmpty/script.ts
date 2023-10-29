5186d53d2911d1de741ec9854121704e60ac5ef4
ruslanyar <styxrus@yandex.ru>
2023-10-29 15:26:48 +0300

    submit 2727

---
+++ leetcode/2727_IsObjectEmpty/script.ts
@@ -0,0 +1,21 @@
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[];
+
+function isEmpty(obj: Obj): boolean {
+  return !Object.keys(obj).length;
+}
+
+console.log(isEmpty([]));
+console.log(isEmpty({}));
