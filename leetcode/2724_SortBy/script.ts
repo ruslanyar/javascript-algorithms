fe6e22e980ddbd056f97f0494f771cbc7635a213
ruslanyar <styxrus@yandex.ru>
2023-10-31 12:30:22 +0300

    submit 2724

---
+++ leetcode/2724_SortBy/script.ts
@@ -0,0 +1,16 @@
+// Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. sortedArray must be sorted in ascending order by fn output.
+
+// You may assume that fn will never duplicate numbers for a given array.
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number;
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+  return arr.sort((a, b) => fn(a) - fn(b));
+}
