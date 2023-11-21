62d1813a5684caa155016836d796e917e978e911
ruslanyar <styxrus@yandex.ru>
2023-11-21 13:20:02 +0300

    submit 2694

---
+++ leetcode/2694_EventEmitter/script.ts
@@ -0,0 +1,53 @@
+// Design an EventEmitter class. This interface is similar (but with some differences) to the one found in Node.js or the Event Target interface of the DOM. The EventEmitter should allow for subscribing to events and emitting them.
+
+// Your EventEmitter class should have the following two methods:
+
+// subscribe - This method takes in two arguments: the name of an event as a string and a callback function. This callback function will later be called when the event is emitted.
+// An event should be able to have multiple listeners for the same event. When emitting an event with multiple callbacks, each should be called in the order in which they were subscribed. An array of results should be returned. You can assume no callbacks passed to subscribe are referentially identical.
+// The subscribe method should also return an object with an unsubscribe method that enables the user to unsubscribe. When it is called, the callback should be removed from the list of subscriptions and undefined should be returned.
+// emit - This method takes in two arguments: the name of an event as a string and an optional array of arguments that will be passed to the callback(s). If there are no callbacks subscribed to the given event, return an empty array. Otherwise, return an array of the results of all callback calls in the order they were subscribed.
+
+type Callback = (...args: any[]) => any;
+type Subscription = {
+  unsubscribe: () => void;
+};
+
+class EventEmitter {
+  private _subscriptions: Map<string, Set<Callback>> = new Map();
+
+  subscribe(eventName: string, callback: Callback): Subscription {
+    let set = this._subscriptions.get(eventName);
+    if (!set) {
+      set = this._subscriptions.set(eventName, new Set()).get(eventName)!;
+    }
+    set.add(callback);
+    return {
+      unsubscribe: () => {
+        set!.delete(callback);
+      },
+    };
+  }
+
+  emit(eventName: string, args: any[] = []): any[] {
+    const result: any[] = [];
+    const set = this._subscriptions.get(eventName);
+    if (set) {
+      set.forEach((fn) => {
+        result.push(fn(...args));
+      });
+    }
+    return result;
+  }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
