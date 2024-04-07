fb40a5f42658262816f9696d8b8d66922a1f7e16
ruslanyar <styxrus@yandex.ru>
2024-04-07 21:29:09 +0300

    add quick sort algo

---
+++ grokking-algorithms/04/quickSort.js
@@ -0,0 +1,19 @@
+function quickSort(arr) {
+  if (arr.length < 2) return arr;
+
+  const pivotIndex = Math.floor(arr.length / 2);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === pivotIndex) continue;
+
+    if (arr[i] < pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([3, 5, 2, 1, 4, 7, 8, 9]));
